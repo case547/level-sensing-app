ab7dafc53ff21a5e5efed7873494e9b2a505c915
case547 <9521840+case547@users.noreply.github.com>
2021-08-04 13:41:54 +0100

    Write code for start()

--- client/app.py
+++ client/app.py
@@ -4,12 +4,44 @@ from kuraconnector import publish_data, run
 from time import time, sleep
 from math import sin, cos
 
+import warnings
+from copy import copy
+from enum import Enum
+
+import numpy as np
+
+import exptool as et
+
 data_interval = 0
+PEAK_MERGE_LIMIT_M = 0.005
 
 def start(get_interval, params):
     global data_interval
     publish_data({"message": "Started with get_interval={:f}, parameters={:s}".format(get_interval, str(params))})
     data_interval = get_interval
+
+    client = et.SocketClient(json_as_py['ip_a']) # Raspberry Pi uses socket client
+    
+    config = et.configs.EnvelopeServiceConfig() # picking envelope service
+
+    sensor_config = get_sensor_config(config)
+    sensor_config.sensor = [1]
+
+    processing_config = get_processing_config()
+    for k, v in json_as_py.items():
+        if hasattr(processing_config, k):
+            try:
+                setattr(processing_config, k, eval(v))
+            except:
+                setattr(processing_config, k, v)
+
+    # Set up session with created config
+    session_info = client.setup_session(sensor_config) # also calls connect()
+    print("Session info:\n", session_info, "\n")
+
+    client.start_session() # call will block until sensor confirms its start
+
+    processor = Processor(sensor_config, processing_config, session_info)
 
 def get(counter):
     global data_interval
